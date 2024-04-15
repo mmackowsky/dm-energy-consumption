8f8424848f09623c4ec0aa3d5bc51f315d93c62c
mmackowsky <maciostah@gmail.com>
2024-04-15 17:40:07 +0200

    config: add new variables

--- src/config.py
+++ src/config.py
@@ -10,6 +10,8 @@ load_dotenv()
 class Settings(BaseSettings):
     SERVICE_HOST: str = os.getenv("SERVICE_HOST")
     SERVICE_PORT: int = os.getenv("SERVICE_PORT")
+    CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL")
+    CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND")
 
     class Config:
         env_file = ".env"
