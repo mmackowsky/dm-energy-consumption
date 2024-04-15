969c0a0375c68a91abcc38e2bfcbb9fd19c9ea1d
mmackowsky <maciostah@gmail.com>
2024-04-15 18:23:30 +0200

    feat(database): update

--- src/database.py
+++ src/database.py
@@ -1,3 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient()
+from config import get_settings
+
+settings = get_settings()
+
+
+client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
