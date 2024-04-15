daacdf382db1e0b38f5eddd606b482d10ccbf310
mmackowsky <maciostah@gmail.com>
2024-04-15 20:28:36 +0200

    feat(database): change db to postgres

--- src/database.py
+++ src/database.py
@@ -1,8 +1,21 @@
-from pymongo import MongoClient
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 from config import get_settings
 
 settings = get_settings()
 
 
-client = MongoClient(settings.MONGO_HOST, settings.MONGO_PORT)
+SQLALCHEMY_DATABASE_URL = settings.SQLALCHEMY_DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
