5512aa273a10c3c6f8437281944a294df4b07ae4
mmackowsky <maciostah@gmail.com>
2024-04-15 17:34:05 +0200

    feat(schemas): update

--- src/schemas.py
+++ src/schemas.py
@@ -5,3 +5,6 @@ class EnergyConsumption(BaseModel):
     id: int
     measurement_date: str
     consumption: float
+
+    def __str__(self):
+        return "%s %s: %s" % (self.id, self.measurement_date, self.consumption)
