bc3445a21445b2a8e82467cbbd7e131238d88220
mmackowsky <maciostah@gmail.com>
2024-04-15 18:23:05 +0200

    config: add new variables

--- src/config.py
+++ src/config.py
@@ -12,6 +12,10 @@ class Settings(BaseSettings):
     SERVICE_PORT: int = os.getenv("SERVICE_PORT")
     CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL")
     CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_COLLECTION: str = os.getenv("DB_COLLECTION")
+    MONGO_HOST: str = os.getenv("MONGO_HOST")
+    MONGO_PORT: int = os.getenv("MONGO_PORT")
 
     class Config:
         env_file = ".env"
