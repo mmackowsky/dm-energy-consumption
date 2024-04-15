3b2195d7c736cac0670e79fd3f897b1cd9941c58
mmackowsky <maciostah@gmail.com>
2024-04-15 20:28:14 +0200

    config: change db to postgres

--- src/config.py
+++ src/config.py
@@ -12,10 +12,12 @@ class Settings(BaseSettings):
     SERVICE_PORT: int = os.getenv("SERVICE_PORT")
     CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL")
     CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND")
-    DB_NAME: str = os.getenv("DB_NAME")
-    DB_COLLECTION: str = os.getenv("DB_COLLECTION")
-    MONGO_HOST: str = os.getenv("MONGO_HOST")
-    MONGO_PORT: int = os.getenv("MONGO_PORT")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    SQLALCHEMY_DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     class Config:
         env_file = ".env"
