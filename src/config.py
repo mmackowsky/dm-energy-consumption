3d9bf57142d611e6fb8e43fb192f31e95b77f603
mmackowsky <maciostah@gmail.com>
2024-04-16 20:35:34 +0200

    config: add new variables

--- src/config.py
+++ src/config.py
@@ -8,8 +8,10 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
+    ALGORITHM: str = os.getenv("ALGORITHM")
     SERVICE_HOST: str = os.getenv("SERVICE_HOST")
     SERVICE_PORT: int = os.getenv("SERVICE_PORT")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     CELERY_BROKER_URL: str = os.getenv("CELERY_BROKER_URL")
     CELERY_RESULT_BACKEND: str = os.getenv("CELERY_RESULT_BACKEND")
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
