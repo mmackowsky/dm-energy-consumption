7d5b5bf4cc95c1d8227158657d9b7c9b7f719de8
mmackowsky <maciostah@gmail.com>
2024-04-17 19:20:13 +0200

    feat(main): add new routes

--- src/main.py
+++ src/main.py
@@ -1,26 +1,43 @@
+from datetime import datetime
 from typing import List
 
 import uvicorn
-from fastapi import FastAPI, Query, status
+from fastapi import FastAPI, Query, Request, status
 
 from config import get_settings
-from schemas import EnergyConsumption
+from database import SessionLocal, engine
+from models import EnergyConsumption
 from utils import get_user_id
 
 settings = get_settings()
 app = FastAPI()
+db = SessionLocal()
+
+
+@app.get("/api/fake-measurement", status_code=status.HTTP_200_OK)
+async def fake_measurement(request: Request):
+    user_id = int(request.headers.get("request-user-id"))
+    energy_consumption = EnergyConsumption(
+        id=1, user=user_id, energy_consumption=100, measurement_date=datetime.now()
+    )
+    db.add(energy_consumption)
+    db.commit()
+    db.refresh(energy_consumption)
+    return energy_consumption
 
 
 @app.get("/api/energy", status_code=status.HTTP_200_OK)
-async def get_measurements():
-    return get_user_id()
+async def get_measurements(request: Request):
+    user_id = request.headers.get("request-user-id")
+    print(user_id)
+    return {"user": user_id}
 
 
 @app.get(
     "/api/energy/{date}",
     status_code=status.HTTP_200_OK,
 )
-def get_measurements(date):
+def get_measurement(request: Request, date):
     pass
 
 
@@ -30,4 +47,5 @@ async def delete_measurement(id):
 
 
 if __name__ == "__main__":
+    EnergyConsumption.metadata.create_all(bind=engine)
     uvicorn.run(app, host=settings.SERVICE_HOST, port=settings.SERVICE_PORT)
