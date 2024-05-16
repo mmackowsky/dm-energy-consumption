cbb7f82a17d51024f1221945cf14e9058a1da671
mmackowsky <maciostah@gmail.com>
2024-05-16 18:56:44 +0200

    feat(models): update

--- src/models.py
+++ src/models.py
@@ -5,8 +5,12 @@ from database import Base
 
 class EnergyConsumption(Base):
     __tablename__ = "energy_consumption"
+    __table_args__ = {"extend_existing": True}
 
     id = Column(Integer, primary_key=True, nullable=False)
     user = Column(Integer, nullable=False)
     measurement_date = Column(String, nullable=False)
     energy_consumption = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"{self.id}, {self.user}, {self.measurement_date}, {self.energy_consumption}"
